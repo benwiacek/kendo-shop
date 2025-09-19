faa66e00c2e36176b9c6b7859e5725bef25b600b
Wiacek <benjamin.wiacek@gmail.com>
2025-09-19 15:24:22 -0400

    add item to the cart function
    Create an array for the cart, push new items to the cart, update quantity and not duplicate items.
    Display cart with item name, quantity and price.
    Total price calculated at the end with reduce() method

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import inventoryArray from "./inventoryArray.js"
 
+/* RENDER INVENTORY */
+
 function getCatalogFeedHtml(arr) {
     return arr.map(item => {
         const { 
@@ -17,11 +19,51 @@ function getCatalogFeedHtml(arr) {
                     <p class="item-materials">${materials.join(", ")}</p>
                     <p class="item-price">$${price}</p>
                 </div>
-                <button class="add-btn">+</button>
+                <button data-add="${id}" class="add-btn">+</button>
             </div>`
     }).join("")
 }
 
-console.log(inventoryArray)
-
 document.getElementById("catalog").innerHTML = getCatalogFeedHtml(inventoryArray)
+
+/* RENDER CART  */
+
+document.addEventListener("click", function(e) {
+    if (e.target.dataset.add){
+        addItemCart(e.target.dataset.add)
+    }
+})
+
+let cartItems = []
+
+function addItemCart(itemId) {
+    const targetItem = inventoryArray.filter(item => item.id === itemId)[0]
+    const cart = document.getElementById("cart")
+    cart.style.display = "block"
+
+    if (!cartItems.some(obj => obj.id === itemId)) {
+        cartItems.push(targetItem)
+        targetItem.quantity = 1
+
+    } else {
+        targetItem.quantity++
+    }
+
+    const totalPrice = cartItems.reduce((total, currentItem) => total + (currentItem.price * currentItem.quantity),0)
+    console.log(totalPrice)
+
+
+    const orderFeed = cartItems.map(targetItem =>`
+        <div>
+            <span class="item-cart">${targetItem.name}</span>
+            <span class="remove">remove</span>
+            <span class="item-cart-quantity">x ${targetItem.quantity}</span>
+            <span class="item-cart-total-price">$${targetItem.price * targetItem.quantity}</span>
+        </div>`
+    ).join("")
+
+    document.getElementById("order").innerHTML = orderFeed
+
+    document.getElementById("amount").innerHTML = totalPrice
+
+}
